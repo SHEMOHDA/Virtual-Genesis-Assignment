14fa97dd08a5f41dfca400c534f7bed29573e7a2
SHEMOHDA <58582455+SHEMOHDA@users.noreply.github.com>
2019-12-08 21:52:35 +0400

    Create Demo.c

---
+++ Demo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int i;
+int main() {
+  int n=10;
+  int arr[10];
+  fill(arr,n);
+  sum=sumarr(arr,n);
+  for(i=0;i<n;i++)
+    printf("%d\n",arr[i]);  //*arr++, *(arr+i)
+  return 0;
+}
+void fill(int* parr,int n) {
+  int i;
+  for(i=0;i<n;i++)
+    parr[i]=i*10+1;  //scanf("%d",&arr[i]);
+    //*parr++=rand()%10;  
+}
+int sumarr(const int* parr, int n) {
+  int i, sum=0;
+  for(i=0;i<n;i++)
+    sum+=*parr++;   //sum+=parr[i]  //sum+=*(parr+i)
+}
